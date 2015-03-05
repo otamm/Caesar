dd8f3948d80e7d30d6b4c14da2c4233054b29840
otamm <oivatom@gmail.com>
2015-03-04 22:40:40 -0300

    Basic RSpec test

---
+++ spec/caesar_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Caesar do
+
+  describe '#encipher' do
+    let(:input) { "IBM" }
+    let(:output) { "HAL" }
+  end
+
+  descripe '#decipher' do
+    #let(:input) { "IBM", "HAL" }
+    #let(:output) { "HAL" }
+  end
+
+end
