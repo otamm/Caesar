3a779511fe659d84fa0a637b7ec5b13d6f7cd720
otamm <oivatom@gmail.com>
2015-03-04 22:49:22 -0300

    Rspec working, sample tests passing.

--- Rakefile
+++ Rakefile
@@ -1,3 +1,4 @@
 require "bundler/gem_tasks"
-Dir.glob('tasks/**/*.rake').each(&method(:import))
+
+Dir.glob('spec/tasks/**/*.rake').each(&method(:import))
 #run RSpec with 'bundle exec rake spec'
--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -3,11 +3,20 @@ require 'spec_helper'
 describe Caesar do
 
   describe '#encipher' do
-    let(:input) { "IBM" }
-    let(:output) { "HAL" }
+
+    it 'encipher with 3 shits by default' do
+      output = Caesar.encipher("IBM")
+      expect(output).to eq "LEP"
+    end
+
+    it 'encipher with arbitrary number of shifts' do
+      output = Caesar.encipher("IBM",25)
+      expect(output).to eq "HAL"
+    end
+
   end
 
-  descripe '#decipher' do
+  describe '#decipher' do
     #let(:input) { "IBM", "HAL" }
     #let(:output) { "HAL" }
   end
