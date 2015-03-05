ef6081d49a8700ccdfce159036f16dd12a6f31b1
otamm <oivatom@gmail.com>
2015-03-05 02:42:10 -0300

    Scratched test for user input with class method

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -4,21 +4,34 @@ describe Caesar do
 
   describe '#encipher' do
 
-    it 'encipher with 3 shits by default' do
+    it 'enciphers with 3 shits by default' do
       output = Caesar.encipher("IBM")
       expect(output).to eq "LEP"
     end
 
-    it 'encipher with arbitrary number of shifts' do
+    it 'enciphers with an arbitrary number of shifts' do
       output = Caesar.encipher("IBM",25)
       expect(output).to eq "HAL"
     end
 
   end
 
-  describe '#decipher' do
-    #let(:input) { "IBM", "HAL" }
-    #let(:output) { "HAL" }
+  #describe '#decipher with positives' do
+    #before do
+    #  $positive = StringIO.new("y\n")
+    #  $negative = StringIO.new("n\n")
+    #end
+
+    #after do
+    #  $positive = STDIN
+    #  $negative = STDIN
+    #end
+
+    #it 'asks user to confirm a deciphered message' do
+    #  positive = StringIO.new("y\n")
+    #  allow(Caesar.decipher("cf")).to receive(:answer) { positive }
+    #  expect(deciphered).to eq "BE"
+    #end
   end
 
 end
